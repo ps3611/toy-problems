3b4720141d11b860504a5e43d1a3138b9de2c29e
Peter Schindler <petertasschindler@gmail.com>
2019-01-14 20:58:06 +0100

    feat: jumping-on-the-clouds

---
+++ src/jumping-on-the-clouds/solution.js
@@ -0,0 +1,14 @@
+function jumpingOnClouds(c) {
+  let jumps = 0;
+  let current = 0;
+  while (current !== c.length-1) {
+    current++;
+    jumps++;
+    if (c[current+1] === 0) current++;
+  }
+  return jumps;
+}
+
+const c = [0, 0, 0, 1, 0, 0];
+const result = jumpingOnClouds(c);
+console.log(result);
