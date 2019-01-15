c2d8eb8cf608edc91223c499d7f4ec03f9bc1e05
Peter Schindler <petertasschindler@gmail.com>
2019-01-15 15:40:13 +0100

    feat: count triplets

---
+++ src/count-triplets/solution.js
@@ -0,0 +1,17 @@
+function countTriplets(arr, r) {
+  let count = 0;
+  for (var i = 0; i < arr.length; i++) {
+    let el = arr[i];
+    let before = arr.slice(0,i);
+    let beforeOkLen = before.filter(x => x === el/r).length;
+    let after = arr.slice(i+1,arr.length);
+    let afterOkLen = after.filter(x => x === el*r).length;
+    count += beforeOkLen*afterOkLen;
+  }
+  return count;
+}
+
+const arr = [1, 3, 9, 9, 27, 81];
+const r = 3;
+const result = countTriplets(arr, r) ;
+console.log(result);
