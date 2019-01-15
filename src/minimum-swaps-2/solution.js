7fe334c5458cdb7684f51718bceab778c40f5949
Peter Schindler <petertasschindler@gmail.com>
2019-01-15 11:40:42 +0100

    feat: minimum-swaps2

---
+++ src/minimum-swaps-2/solution.js
@@ -0,0 +1,22 @@
+function swap(arr, i, j){
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+  return arr;
+}
+
+function minimumSwaps(arr) {
+  let swaps = 0;
+  for (let el = 1; el <= arr.length; el++) {
+    let curr_i = arr.indexOf(el);
+    if (el !== curr_i+1) {
+      arr = swap(arr,curr_i,el-1);
+      swaps++;
+    }
+  }
+  return swaps;
+}
+
+const arr = [7, 1, 3, 2, 4, 5, 6];
+const result = minimumSwaps(arr);
+console.log(result);
